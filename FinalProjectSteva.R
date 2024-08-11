057b41672a55a641135a611ca66d512c9bd03aeb
caseysteva <caseysteva@gmail.com>
2024-08-11 08:06:37 -0700

    Found DataSet to work on

--- FinalProjectSteva.R
+++ FinalProjectSteva.R
@@ -1,11 +1,11 @@
----
-  title:  'Casey Steva Final Project'
+
+  "title:  'Casey Steva Final Project'
 subtitle: 'INFO 523'
 author:
   - name: Student -  [Casey Steva]
 affiliation: [University of Arizona, Grad Student]
 - name: Instructor -  Cristian Román-Palacios
-affiliation: School of Information, University of Arizona, Tucson, AZ
+affiliation: School of Information, University of Arizona, Tucson, AZ"
 
 
 
@@ -16,6 +16,16 @@ affiliation: School of Information, University of Arizona, Tucson, AZ
 
   
   #### Additional resources relevant to this Final
+#Data was obtained from https://catalog.data.gov/dataset/supply-chain-greenhouse-gas-emission-factors-v1-2-by-naics-6/resource/fbc78d3c-49bd-40c0-9dac-2ed16c07a305
+#Dataset called ""SupplyChainGHGEmissionFactors_v1.2_NAICS_byGHG_USD2021.csv"
+#install.packages("rpart")
+#install.packages("rpart.plot")
+library(rpart)
+library(rpart.plot)
+
+url <- "https://catalog.data.gov/dataset/supply-chain-greenhouse-gas-emission-factors-v1-2-by-naics-6/resource/fbc78d3c-49bd-40c0-9dac-2ed16c07a305"
+
+SupplyData <- read.csv("SupplyChainGHGEmissionFactors_v1.2_NAICS_byGHG_USD2021.csv")
 
 
 # BEGIN SOLUTION
