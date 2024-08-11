b986d3754a7b2c7bb1bb3c8395358888d68bad94
caseysteva <caseysteva@gmail.com>
2024-08-11 07:34:36 -0700

    Starting Final Project

---
+++ FinalProjectSteva.R
@@ -0,0 +1,22 @@
+---
+  title:  'Casey Steva Final Project'
+subtitle: 'INFO 523'
+author:
+  - name: Student -  [Casey Steva]
+affiliation: [University of Arizona, Grad Student]
+- name: Instructor -  Cristian Román-Palacios
+affiliation: School of Information, University of Arizona, Tucson, AZ
+
+
+
+  
+  ### Objectives
+  #This Final Project focuses on Geopolitical data using CART 
+
+
+  
+  #### Additional resources relevant to this Final
+
+
+# BEGIN SOLUTION
+q1 <- NULL
